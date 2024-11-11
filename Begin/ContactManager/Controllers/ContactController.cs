3c0e02d6ba1b97399f32e1b1c6cbf626feea2a61
helena.borzan <helena.borzan@gmail.com>
2024-11-11 15:02:03 +0100

    Extracting Functionality into a Service Layer

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -1,6 +1,7 @@
 ﻿using ContactManager.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ContactManager.Services;
 
 namespace ContactManager.Controllers
 {
@@ -8,16 +9,17 @@ namespace ContactManager.Controllers
     [ApiController]
     public class ContactController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<Contact>> Get()
-        {
-            var contacts = new List<Contact>
-            {
-                new Contact { Id = 1, Name = "Glenn Block" },
-                new Contact { Id = 2, Name = "Dan Roth" }
-            };
+        private ContactRepository contactRepository;
 
-            return Ok(contacts);
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();  // Tightly coupled to the repository
+        }
+
+        [HttpGet]
+        public IEnumerable<Contact> GetAllContacts()
+        {
+            return this.contactRepository.GetAllContacts();
         }
     }
 }
---
+++ Begin/ContactManager/Services/ContactRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using ContactManager.Models;
+namespace ContactManager.Services
+{
+    public class ContactRepository
+    {
+        public IEnumerable<Contact> GetAllContacts()
+        {
+            return new List<Contact>
+            {
+                new Contact { Id = 1, Name = "Glenn Block" },
+                new Contact { Id = 2, Name = "Dan Roth" }
+            };
+        }
+    }
+}
