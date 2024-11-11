d83f3670a76a520e04dc27d3065640338e1d5089
helena.borzan <helena.borzan@gmail.com>
2024-11-11 14:25:09 +0100

    Creating the Contact Manager API Controllers

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string[]> Get()
+        {
+            return new string[] { "Hello", "World" };
+        }
+    }
+}
--- Begin/ContactManager/Program.cs
+++ Begin/ContactManager/Program.cs
@@ -4,6 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()   // Allows any origin
+              .AllowAnyMethod()   // Allows any HTTP method (GET, POST, etc.)
+              .AllowAnyHeader();  // Allows any header
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
